377bd2ab17bc784d85b0e17c2a89cacabe0ceaec
DaisukeMukumoto <daisuke.mukumoto@gmail.com>
2020-08-13 07:34:45 +0000

    Change docs

--- docs/sw.js
+++ docs/sw.js
@@ -28,5 +28,5 @@ workbox.precaching.cleanupOutdatedCaches()
 // --------------------------------------------------
 
 // Register route handlers for runtimeCaching
-workbox.routing.registerRoute(new RegExp('/_nuxt/'), new workbox.strategies.CacheFirst ({}), 'GET')
-workbox.routing.registerRoute(new RegExp('/'), new workbox.strategies.NetworkFirst ({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/nuxt_pwa_sample/_nuxt/'), new workbox.strategies.CacheFirst ({}), 'GET')
+workbox.routing.registerRoute(new RegExp('/nuxt_pwa_sample/'), new workbox.strategies.NetworkFirst ({}), 'GET')
